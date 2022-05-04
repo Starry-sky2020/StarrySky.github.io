450d49edd2fc41ca2b5d23617e7580f6eb746119
Starry-sky2020 <80570818+Starry-sky2020@users.noreply.github.com>
2022-05-05 00:49:34 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某|某|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||哥|哥|祝|你|生|日|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
